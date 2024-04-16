0c8089edaef9c3f8baad97e8fa01edeabb9c06d0
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-16 17:24:39 -0400

    Update base-search.js

--- src/js/core/base-search.js
+++ src/js/core/base-search.js
@@ -4,23 +4,24 @@
  */
 
 import config from './../config.js';
-import Dialog_class from './../libs/popup.js';
-import Base_gui_class from './base-gui.js';
+import DialogClass from './../libs/popup.js';
+import BaseGUIClass from './base-gui.js';
 const fuzzysort = require('fuzzysort');
 
 var instance = null;
 
-class Base_search_class {
+class BaseSearchClass {
 
 	constructor() {
-		//singleton
+		// Singleton
 		if (instance) {
 			return instance;
 		}
+		
 		instance = this;
 
-		this.POP = new Dialog_class();
-		this.Base_gui = new Base_gui_class();
+		this.POP = new DialogClass();
+		this.BaseGUI = new BaseGUIClass();
 		this.db = null;
 
 		this.events();
@@ -28,20 +29,21 @@ class Base_search_class {
 
 	events() {
 		document.addEventListener('keydown', (event) => {
-			if (this.POP.get_active_instances() > 0) {
+			if (this.POP.getActiveInstances() > 0) {
 				return;
 			}
 
 			var code = event.key;
+			
 			if (code == "F3" || ( (event.ctrlKey == true || event.metaKey) && code == "f")) {
-				//open
+				// Open
 				this.search();
 				event.preventDefault();
 			}
 		}, false);
 
 		document.addEventListener('input', (event) => {
-			if(document.querySelector('#pop_data_search') == null){
+			if (document.querySelector('#pop_data_search') == null) {
 				return;
 			}
 
@@ -49,7 +51,8 @@ class Base_search_class {
 			node.innerHTML = '';
 
 			var query = event.target.value;
-			if(query == ''){
+			
+			if (query == '') {
 				return;
 			}
 
@@ -59,46 +62,51 @@ class Base_search_class {
 				threshold: -50000,
 			});
 
-			//show
-			for(var i = 0; i < results.length; i++) {
+			// Show
+			for (var i = 0; i < results.length; i++) {
 				var item = results[i];
 
-				var className = "search-result n" + (i+1);
-				if(i == 0){
+				var className = "search-result n" + (i + 1);
+				
+				if (i == 0) {
 					className += " active";
 				}
 
-				node.innerHTML += "<div class='"+className+"' data-key='"+item.obj.key+"'>"
+				node.innerHTML += "<div class='" + className + "' data-key='" + item.obj.key + "'>"
 					+ fuzzysort.highlight(item[0]) + "</div>";
 			}
 		}, false);
 
-		//allow to select with arrow keys
-		document.addEventListener('keydown', function (e) {
-			if(document.querySelector('#global_search_results') == null
-				|| document.querySelector('.search-result') == null){
+		// Allow to select with arrow keys
+		document.addEventListener('keydown', function(e) {
+			if (document.querySelector('#global_search_results') == null
+				|| document.querySelector('.search-result') == null) {
 				return;
 			}
+			
 			var k = e.key;
 
 			if (k == "ArrowUp") {
 				var target = document.querySelector('.search-result.active');
 				var index = Array.from(target.parentNode.children).indexOf(target);
-				if(index > 0){
+				
+				if (index > 0) {
 					index--;
 				}
+				
 				target.classList.remove('active');
 				var target2 =document.querySelector('#global_search_results').childNodes[index];
 				target2.classList.add('active');
 				e.preventDefault();
-			}
-			else if (k == "ArrowDown") {
+			} else if (k == "ArrowDown") {
 				var target = document.querySelector('.search-result.active');
 				var index = Array.from(target.parentNode.children).indexOf(target);
 				var total = target.parentNode.childElementCount;
-				if(index < total - 1){
+				
+				if (index < total - 1) {
 					index++;
 				}
+				
 				target.classList.remove('active');
 				var target2 = document.querySelector('#global_search_results').childNodes[index];
 				target2.classList.add('active');
@@ -112,9 +120,10 @@ class Base_search_class {
 		var _this = this;
 
 		//init DB
-		if(this.db === null) {
-			this.db = Object.keys(this.Base_gui.modules);
-			for(var i in this.db){
+		if (this.db === null) {
+			this.db = Object.keys(this.BaseGUI.modules);
+			
+			for (var i in this.db) {
 				this.db[i] = {
 					key: this.db[i],
 					title: this.db[i].replace(/_/i, ' '),
@@ -134,26 +143,28 @@ class Base_search_class {
 				popup.el.querySelector('.dialog_content').appendChild(node);
 			},
 			on_finish: function (params) {
-				//execute
+				// Execute
 				var target = document.querySelector('.search-result.active');
-				if(target){
-					//execute
+				
+				if (target) {
+					// Execute
 					var key = target.dataset.key;
-					var class_object = this.Base_gui.modules[key];
-					var function_name = _this.get_function_from_path(key);
+					var classObject = this.BaseGUI.modules[key];
+					var functionName = _this.getFunctionFromPath(key);
 
 					_this.POP.hide();
-					class_object[function_name]();
+					classObject[functionName]();
 				}
 			},
 		};
+		
 		this.POP.show(settings);
 
-		//on input change
+		// On input change
 		document.getElementById("pop_data_search").select();
 	}
 
-	get_function_from_path(path){
+	getFunctionFromPath(path) {
 		var parts = path.split("/");
 		var result = parts[parts.length - 1];
 		result = result.replace(/-/, '_');
