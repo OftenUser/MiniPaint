e4af5bebce99b8eaafaa91ffcba6c31d552fdd89
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-16 17:16:45 -0400

    Update swatches.js

--- src/js/core/components/swatches.js
+++ src/js/core/components/swatches.js
@@ -7,46 +7,46 @@
 		</div>
 	`;
 
-	const on_key_down_swatches = (event) => {
+	const onKeyDownSwatches = (event) => {
 		const $el = $(event.target.closest('.ui_swatches'));
 		const key = event.key;
-		const { rows, count, selectedIndex } = $el.data();
+		const {rows, count, selectedIndex} = $el.data();
+		
 		if (['Left', 'ArrowLeft'].includes(key)) {
 			event.preventDefault();
-			set_selected_index($el, selectedIndex - 1);
+			setSelectedIndex($el, selectedIndex - 1);
 			$el.trigger('input');
-		}
-		else if (['Right', 'ArrowRight'].includes(key)) {
+		} else if (['Right', 'ArrowRight'].includes(key)) {
 			event.preventDefault();
-			set_selected_index($el, selectedIndex + 1);
+			setSelectedIndex($el, selectedIndex + 1);
 			$el.trigger('input');
-		}
-		else if (['Up', 'ArrowUp'].includes(key)) {
+		} else if (['Up', 'ArrowUp'].includes(key)) {
 			event.preventDefault();
-			set_selected_index($el, selectedIndex - Math.floor(count / rows));
+			setSelectedIndex($el, selectedIndex - Math.floor(count / rows));
 			$el.trigger('input');
-		}
-		else if (['Down', 'ArrowDown'].includes(key)) {
+		} else if (['Down', 'ArrowDown'].includes(key)) {
 			event.preventDefault();
-			set_selected_index($el, selectedIndex + Math.floor(count / rows));
+			setSelectedIndex($el, selectedIndex + Math.floor(count / rows));
 			$el.trigger('input');
 		}
 	};
 
-	const on_click_swatches = (event) => {
+	const onClickSwatches = (event) => {
 		const target = event.target;
 		const $el = $(target.closest('.ui_swatches'));
+		
 		if (target.classList.contains('swatch')) {
-			const { swatches } = $el.data();
-			set_selected_index($el, swatches.indexOf(target));
+			const {swatches} = $el.data();
+			setSelectedIndex($el, swatches.indexOf(target));
 			$el.trigger('input');
 		}
 	};
 
-	const set_selected_index = ($el, index) => {
-		const { readonly, swatches } = $el.data();
+	const setSelectedIndex = ($el, index) => {
+		const {readonly, swatches} = $el.data();
 		if (swatches[index]) {
 			$el.data('selectedIndex', index);
+			
 			if (!readonly) {
 				$el.find('.active').removeClass('active');
 				$(swatches[index]).addClass('active');
@@ -54,8 +54,9 @@
 		}
 	};
 
-	const set_selected_hex = ($el, hex) => {
-		const { selectedIndex, swatches } = $el.data();
+	const setSelectedHex = ($el, hex) => {
+		const {selectedIndex, swatches} = $el.data();
+		
 		if (/^\#[0-9A-F]{6}$/gi.test(hex)) {
 			const swatch = swatches[selectedIndex];
 			$(swatch)
@@ -64,12 +65,14 @@
 		}
 	};
 
-	const set_all_hex = ($el, hexArray) => {
+	const setAllHex = ($el, hexArray) => {
 		hexArray = hexArray || [];
-		const { swatches } = $el.data();
+		const {swatches} = $el.data();
+		
 		for (let i = 0; i < swatches.length; i++) {
 			if (hexArray[i]) {
 				const hex = hexArray[i];
+				
 				if (/^\#[0-9A-F]{6}$/gi.test(hex)) {
 					$(swatches[i])
 						.data('hex', hex)
@@ -110,18 +113,22 @@
 				if (id) {
 					el.setAttribute('id', id);
 				}
+				
 				if (cols) {
 					swatchGroup.classList.add('cols_' + cols);
 				}
+				
 				swatchGroup.classList.add('rows_' + rows);
 
 				const swatches = [];
+				
 				for (let i = 0; i < count; i++) {
 					const swatch = document.createElement('div');
 					swatch.classList.add('swatch');
-					$(swatch).data('hex', '#ffffff');
+					$(swatch).data('hex', '#FFFFFF');
 					swatches.push(swatch);
 					swatchGroup.appendChild(swatch);
+					
 					if (i === selectedIndex && !readonly) {
 						swatch.classList.add('active');
 					}
@@ -138,28 +145,27 @@
 				});
 
 				$el
-					.on('click', on_click_swatches)
-					.on('keydown', on_key_down_swatches);
+					.on('click', onClickSwatches)
+					.on('keydown', onKeyDownSwatches);
 			}
 			// Behaviors
-			else if (behavior === 'set_selected_hex') {
+			else if (behavior === 'setSelectedHex') {
 				const newValue = args[0] + '';
-				set_selected_hex($(el), newValue);
-			}
-			else if (behavior === 'get_selected_hex') {
+				setSelectedHex($(el), newValue);
+			} else if (behavior === 'getSelectedHex') {
 				const { selectedIndex, swatches } = $(el).data();
 				returnValues.push($(swatches[selectedIndex]).data('hex'));
-			}
-			else if (behavior === 'set_all_hex') {
-				set_all_hex($(el), args[0]);
-			}
-			else if (behavior === 'get_all_hex') {
-				const { swatches } = $(el).data();
+			} else if (behavior === 'setAllHex') {
+				setAllHex($(el), args[0]);
+			} else if (behavior === 'getAllHex') {
+				const {swatches} = $(el).data();
+				
 				for (let swatch of swatches) {
 					returnValues.push($(swatch).data('hex'));
 				}
 			}
 		}
+		
 		if (returnValues.length > 0) {
 			return returnValues.length === 1 ? returnValues[0] : returnValues;
 		} else {
