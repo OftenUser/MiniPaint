ea63ed59aa0925cbca2289c60a460020f6f425d7
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-16 15:42:59 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
  * author: Vilius L.
  */
 
-//css
+// CSS
 import './../css/reset.css';
 import './../css/utility.css';
 import './../css/component.css';
@@ -11,45 +11,46 @@ import './../css/layout.css';
 import './../css/menu.css';
 import './../css/print.css';
 import './../../node_modules/alertifyjs/build/css/alertify.min.css';
-//js
+
+// JavaScript
 import app from './app.js';
 import config from './config.js';
 import './core/components/index.js';
-import Base_gui_class from './core/base-gui.js';
-import Base_layers_class from './core/base-layers.js';
-import Base_tools_class from './core/base-tools.js';
-import Base_state_class from './core/base-state.js';
-import Base_search_class from './core/base-search.js';
-import File_open_class from './modules/file/open.js';
-import File_save_class from './modules/file/save.js';
+import BaseGUIClass from './core/base-gui.js';
+import BaseLayersClass from './core/base-layers.js';
+import BaseToolsClass from './core/base-tools.js';
+import BaseStateClass from './core/base-state.js';
+import BaseSearchClass from './core/base-search.js';
+import FileOpenClass from './modules/file/open.js';
+import FileSaveClass from './modules/file/save.js';
 import * as Actions from './actions/index.js';
 
-window.addEventListener('load', function (e) {
+window.addEventListener('load', function(e) {
 	// Initiate app
-	var Layers = new Base_layers_class();
-	var Base_tools = new Base_tools_class(true);
-	var GUI = new Base_gui_class();
-	var Base_state = new Base_state_class();
-	var File_open = new File_open_class();
-	var File_save = new File_save_class();
-	var Base_search = new Base_search_class();
+	var Layers = new BaseLayersClass();
+	var BaseTools = new BaseToolsClass(true);
+	var GUI = new BaseGUIClass();
+	var BaseState = new BaseStateClass();
+	var FileOpen = new FileOpenClass();
+	var FileSave = new FileSaveClass();
+	var BaseSearch = new BaseSearchClass();
 
 	// Register singletons in app module
 	app.Actions = Actions;
 	app.Config = config;
-	app.FileOpen = File_open;
-	app.FileSave = File_save;
+	app.FileOpen = FileOpen;
+	app.FileSave = FileSave;
 	app.GUI = GUI;
 	app.Layers = Layers;
-	app.State = Base_state;
-	app.Tools = Base_tools;
+	app.State = BaseState;
+	app.Tools = BaseTools;
 
 	// Register as global for quick or external access
 	window.Layers = Layers;
 	window.AppConfig = config;
-	window.State = Base_state;
-	window.FileOpen = File_open;
-	window.FileSave = File_save;
+	window.State = BaseState;
+	window.FileOpen = FileOpen;
+	window.FileSave = FileSave;
 
 	// Render all
 	GUI.init();
