c7172e62e28c7b1b6a2afa8f3fc89b598d7c7387
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-18 16:05:42 -0400

    Update add-layer-filter.js

--- src/js/actions/add-layer-filter.js
+++ src/js/actions/add-layer-filter.js
@@ -1,67 +1,72 @@
 import app from './../app.js';
 import config from './../config.js';
-import { Base_action } from './base.js';
+import {BaseAction} from './base.js';
 
-export class Add_layer_filter_action extends Base_action {
+export class AddLayerFilterAction extends BaseAction {
 	/**
-	 * register new live filter
+	 * Register new live filter
 	 *
 	 * @param {int} layer_id
 	 * @param {string} name
 	 * @param {object} params
 	 */
-	constructor(layer_id, name, params, filter_id) {
+	constructor(layerID, name, params, filterID) {
 		super('add_layer_filter', 'Add Layer Filter');
-		if (layer_id == null)
-			layer_id = config.layer.id;
-		this.layer_id = parseInt(layer_id);
+		if (layerID == null)
+			layerID = config.layer.id;
+		this.layerID = parseInt(layer_id);
 		this.name = name;
 		this.params = params;
-		this.filter_id = filter_id;
-		this.reference_layer = null;
+		this.filterID = filterID;
+		this.referenceLayer = null;
 	}
 
 	async do() {
 		super.do();
-		this.reference_layer = app.Layers.get_layer(this.layer_id);
-		if (!this.reference_layer) {
-			throw new Error('Aborted - layer with specified id doesn\'t exist');
+		this.referenceLayer = app.Layers.getLayer(this.layerID);
+		
+		if (!this.referenceLayer) {
+			throw new Error('Aborted - Layer with specified ID doesn\'t exist');
 		}
+		
 		var filter = {
-			id: this.filter_id,
+			id: this.filterID,
 			name: this.name,
 			params: this.params,
 		};
-		if(this.filter_id) {
-			//update
-			for(var i in this.reference_layer.filters) {
-				if(this.reference_layer.filters[i].id == this.filter_id){
-					this.reference_layer.filters[i] = filter;
+		
+		if (this.filterID) {
+			// Update
+			for (var i in this.referenceLayer.filters) {
+				if(this.referenceLayer.filters[i].id == this.filterID) {
+					this.referenceLayer.filters[i] = filter;
 					break;
 				}
 			}
-		}
-		else{
-			//insert
+		} else {
+			// Insert
 			filter.id = Math.floor(Math.random() * 999999999) + 1; // A good UUID library would
-			this.reference_layer.filters.push(filter);
+			this.referenceLayer.filters.push(filter);
 		}
-		config.need_render = true;
-		app.GUI.GUI_layers.render_layers();
+		
+		config.needRender = true;
+		app.GUI.GUILayers.renderLayers();
 	}
 
 	async undo() {
 		super.undo();
-		if (this.reference_layer) {
-			this.reference_layer.filters.pop();
-			this.reference_layer = null;
+		
+		if (this.referenceLayer) {
+			this.referenceLayer.filters.pop();
+			this.referenceLayer = null;
 		}
+		
 		config.need_render = true;
-		app.GUI.GUI_layers.render_layers();
+		app.GUI.GUILayers.renderLayers();
 	}
 
 	free() {
-		this.reference_layer = null;
+		this.referenceLayer = null;
 		this.params = null;
 	}
 }
