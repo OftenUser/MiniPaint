6c588b93a0535ba4db6c01921caf5b633912ecb2
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-18 19:57:04 -0400

    Update update-layer.js

--- src/js/actions/update-layer.js
+++ src/js/actions/update-layer.js
@@ -1,8 +1,8 @@
 import app from './../app.js';
 import config from './../config.js';
-import { Base_action } from './base.js';
+import {BaseAction} from './base.js';
 
-export class Update_layer_action extends Base_action {
+export class UpdateLayerAction extends BaseAction {
 	/**
 	 * Updates an existing layer with the provided settings
 	 * WARNING: If passing objects or arrays into settings, make sure these are new or cloned objects, and not a modified existing object!
@@ -10,58 +10,70 @@ export class Update_layer_action extends Base_action {
 	 * @param {string} layer_id
 	 * @param {object} settings 
 	 */
-	constructor(layer_id, settings) {
+	constructor(layerID, settings) {
 		super('update_layer', 'Update Layer');
-		this.layer_id = layer_id;
+		this.layerID = layerID;
 		this.settings = settings;
-		this.reference_layer = null;
-		this.old_settings = {};
+		this.referenceLayer = null;
+		this.oldSettings = {};
 	}
 
 	async do() {
 		super.do();
-		this.reference_layer = app.Layers.get_layer(this.layer_id);
+		this.referenceLayer = app.Layers.getLayer(this.layerID);
+		
 		if (!this.reference_layer) {
-			throw new Error('Aborted - layer with specified id doesn\'t exist');
+			throw new Error('Aborted - Layer with specified ID doesn\'t exist');
 		}
+		
 		for (let i in this.settings) {
 			if (i == 'id')
 				continue;
+			
 			if (i == 'order')
 				continue;
-			this.old_settings[i] = this.reference_layer[i];
-			this.reference_layer[i] = this.settings[i];
+			
+			this.oldSettings[i] = this.referenceLayer[i];
+			this.referenceLayer[i] = this.settings[i];
 		}
-		if (this.reference_layer.type === 'text') {
-			this.reference_layer._needs_update_data = true;
+		
+		if (this.referenceLayer.type === 'text') {
+			this.referenceLayer.needsUpdateData = true;
 		}
+		
 		if (this.settings.params || this.settings.width || this.settings.height) {
-			config.need_render_changed_params = true;
+			config.needRenderChangedParams = true;
 		}
-		config.need_render = true;
+		
+		config.needRender = true;
 	}
 
 	async undo() {
 		super.undo();
-		if (this.reference_layer) {
-			for (let i in this.old_settings) {
-				this.reference_layer[i] = this.old_settings[i];
+		
+		if (this.referenceLayer) {
+			for (let i in this.oldSettings) {
+				this.referenceLayer[i] = this.oldSettings[i];
 			}
-			if (this.reference_layer.type === 'text') {
-				this.reference_layer._needs_update_data = true;
+			
+			if (this.referenceLayer.type === 'text') {
+				this.referenceLayer.needsUpdateData = true;
 			}
-			if (this.old_settings.params || this.old_settings.width || this.old_settings.height) {
-				config.need_render_changed_params = true;
+			
+			if (this.oldSettings.params || this.oldSettings.width || this.oldSettings.height) {
+				config.needRenderChangedParams = true;
 			}
+			
 			this.old_settings = {};
 		}
-		this.reference_layer = null;
-		config.need_render = true;
+		
+		this.referenceLayer = null;
+		config.needRender = true;
 	}
 
 	free() {
 		this.settings = null;
-		this.old_settings = null;
-		this.reference_layer = null;
+		this.oldSettings = null;
+		this.referenceLayer = null;
 	}
 }
