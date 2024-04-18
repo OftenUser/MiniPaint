d687118105300e7407018b20777dfff67a3c33c5
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-18 17:56:04 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,11 @@
+// IMPORTANT - This file is not used!
 
-//IMPORTANT - this file is not used !!!
-
-
-// use a cacheName for cache versioning
+// Use a cacheName for cache versioning
 var cacheName = 'v1:static';
 
-// during the install phase you usually want to cache static assets
+// During the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
-	// once the SW is installed, go ahead and fetch the resources to make this work offline
+	// Once the SW is installed, go ahead and fetch the resources to make this work offline
 	e.waitUntil(
 		caches.open(cacheName).then(function(cache) {
 			return cache.addAll([
@@ -52,16 +50,16 @@ self.addEventListener('install', function(e) {
 	);
 });
 
-// when the browser fetches a url
+// When the browser fetches a URL
 self.addEventListener('fetch', function(event) {
-	// either respond with the cached object or go ahead and fetch the actual url
+	// Either respond with the cached object or go ahead and fetch the actual url
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
 			if (response) {
-				// retrieve from cache
+				// Retrieve from cache
 				return response;
 			}
-			// fetch as normal
+			// Fetch as normal
 			return fetch(event.request);
 		})
 	);
