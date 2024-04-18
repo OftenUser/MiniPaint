23c6988f3c234cb62ec226e0bac657960ff5de20
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-18 16:06:32 -0400

    Update base.js

--- src/js/actions/base.js
+++ src/js/actions/base.js
@@ -1,18 +1,20 @@
-
-export class Base_action {
-	constructor(action_id, action_description) {
-		this.action_id = action_id;
-		this.action_description = action_description;
-		this.is_done = false;
-		this.memory_estimate = 0; // Estimate of how much memory will be freed when the free() method is called (in bytes)
-		this.database_estimate = 0; // Estimate of how much database space will be freed when the free() method is called (in bytes)
+export class BaseAction {
+	constructor(actionID, actionDescription) {
+		this.actionID = actionID;
+		this.actionDescription = actionDescription;
+		this.isDone = false;
+		this.memoryEstimate = 0; // Estimate of how much memory will be freed when the free() method is called (in bytes)
+		this.databaseEstimate = 0; // Estimate of how much database space will be freed when the free() method is called (in bytes)
 	}
+	
 	do() {
-		this.is_done = true;
+		this.isDone = true;
 	}
+	
 	undo() {
-		this.is_done = false;
+		this.isDone = false;
 	}
+	
 	free() {
 		// Override if need to run tasks to free memory when action is discarded from history
 	}
