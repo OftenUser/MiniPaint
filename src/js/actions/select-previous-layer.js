53c09d3d8d086c994bd07e8e284da8c7488b13d0
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-19 15:32:06 -0400

    Update select-previous-layer.js

--- src/js/actions/select-previous-layer.js
+++ src/js/actions/select-previous-layer.js
@@ -1,33 +1,35 @@
 import app from './../app.js';
 import config from './../config.js';
-import { Base_action } from './base.js';
+import {BaseAction} from './base.js';
 
-export class Select_previous_layer_action extends Base_action {
-	constructor(reference_layer_id) {
+export class SelectPreviousLayerAction extends BaseAction {
+	constructor(referenceLayerID) {
 		super('select_previous_layer', 'Select Previous Layer');
-		this.reference_layer_id = reference_layer_id;
-		this.old_config_layer = null;
+		this.referenceLayerID = referenceLayerID;
+		this.oldConfigLayer = null;
 	}
 
 	async do() {
 		super.do();
-		const previous_layer = app.Layers.find_previous(this.reference_layer_id);
-		if (!previous_layer) {
+		const previousLayer = app.Layers.findPrevious(this.referenceLayerID);
+		
+		if (!previousLayer) {
 			throw new Error('Aborted - Previous layer to select not found');
 		}
-		this.old_config_layer = config.layer;
-		config.layer = previous_layer;
+		
+		this.oldConfigLayer = config.layer;
+		config.layer = previousLayer;
 
 		app.Layers.render();
-		app.GUI.GUI_layers.render_layers();
+		app.GUI.GUILayers.renderLayers();
 	}
 
 	async undo() {
 		super.undo();
-		config.layer = this.old_config_layer;
-		this.old_config_layer = null;
+		config.layer = this.oldConfigLayer;
+		this.oldConfigLayer = null;
 
 		app.Layers.render();
-		app.GUI.GUI_layers.render_layers();
+		app.GUI.GUILayers.renderLayers();
 	}
 }
