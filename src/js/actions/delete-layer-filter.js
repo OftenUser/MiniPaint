18a02a58250bea68c61c8d0da66208017b19f579
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-19 15:38:15 -0400

    Update delete-layer-filter.js

--- src/js/actions/delete-layer-filter.js
+++ src/js/actions/delete-layer-filter.js
@@ -1,60 +1,69 @@
 import app from './../app.js';
 import config from './../config.js';
-import { Base_action } from './base.js';
+import {BaseAction} from './base.js';
 
-export class Delete_layer_filter_action extends Base_action {
+export class DeleteLayerFilterAction extends BaseAction {
 	/**
-	 * delete live filter
+	 * Delete live filter
 	 *
-	 * @param {int} layer_id
-	 * @param {string} filter_id
+	 * @param {int} layerID
+	 * @param {string} filterID
 	 */
-	constructor(layer_id, filter_id) {
+	constructor(layerID, filterID) {
 		super('delete_layer_filter', 'Delete Layer Filter');
-		if (layer_id == null)
-			layer_id = config.layer.id;
-		this.layer_id = parseInt(layer_id);
-		this.filter_id = filter_id;
-		this.reference_layer = null;
-		this.filter_remove_index = null;
-		this.old_filter = null;
+		
+		if (layerID == null)
+			layerID = config.layer.id;
+		
+		this.layerID = parseInt(layerID);
+		this.filterID = filterID;
+		this.referenceLayer = null;
+		this.filterRemoveIndex = null;
+		this.oldFilter = null;
 	}
 
 	async do() {
 		super.do();
-		this.reference_layer = app.Layers.get_layer(this.layer_id);
-		if (!this.reference_layer) {
-			throw new Error('Aborted - layer with specified id doesn\'t exist');
+		this.referenceLayer = app.Layers.getLayer(this.layerID);
+		
+		if (!this.referenceLayer) {
+			throw new Error('Aborted - Layer with specified ID doesn\'t exist');
 		}
-		this.old_filter = null;
-		for (let i in this.reference_layer.filters) {
-			if (this.reference_layer.filters[i].id == this.filter_id) {
-				this.filter_remove_index = i;
-				this.old_filter = this.reference_layer.filters.splice(i, 1)[0];
+		
+		this.oldFilter = null;
+		
+		for (let i in this.referenceLayer.filters) {
+			if (this.referenceLayer.filters[i].id == this.filterID) {
+				this.filterRemoveIndex = i;
+				this.oldFilter = this.referenceLayer.filters.splice(i, 1)[0];
 				break;
 			}
 		}
-		if (!this.old_filter) {
-			throw new Error('Aborted - filter with specified id doesn\'t exist in layer');
+		
+		if (!this.oldFilter) {
+			throw new Error('Aborted - Filter with specified ID doesn\'t exist in layer');
 		}
-		config.need_render = true;
-		app.GUI.GUI_layers.render_layers();
+		
+		config.needRender = true;
+		app.GUI.GUILayers.renderLayers();
 	}
 
 	async undo() {
 		super.undo();
-		if (this.reference_layer && this.old_filter) {
-			this.reference_layer.filters.splice(this.filter_remove_index, 0, this.old_filter);
+		
+		if (this.referenceLayer && this.oldFilter) {
+			this.referenceLayer.filters.splice(this.filterRemoveIndex, 0, this.oldFilter);
 		}
-		this.reference_layer = null;
-		this.old_filter = null;
-		this.filter_remove_index = null;
-		config.need_render = true;
-		app.GUI.GUI_layers.render_layers();
+		
+		this.referenceLayer = null;
+		this.oldFilter = null;
+		this.filterRemoveIndex = null;
+		config.needRender = true;
+		app.GUI.GUILayers.renderLayers();
 	}
 
 	free() {
-		this.reference_layer = null;
-		this.old_filter = null;
+		this.referenceLayer = null;
+		this.oldFilter = null;
 	}
 }
