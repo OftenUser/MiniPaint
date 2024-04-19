33abf8a3607cc56c6ea88f29540d73189f667cd2
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-19 15:28:45 -0400

    Update refresh-layers-gui.js

--- src/js/actions/refresh-layers-gui.js
+++ src/js/actions/refresh-layers-gui.js
@@ -1,29 +1,31 @@
 import app from '../app.js';
 import config from '../config.js';
-import { Base_action } from './base.js';
+import {BaseAction} from './base.js';
 
-export class Refresh_layers_gui_action extends Base_action {
+export class RefreshLayersGUIAction extends BaseAction {
 	/**
 	 * Resizes/renders the canvas at the specified step. Usually used on both sides of a config update action.
 	 *
 	 * @param {boolean} call_when
 	 */
-	constructor(call_when = 'undo') {
+	constructor(callWhen = 'undo') {
 		super('refresh_gui', 'Refresh GUI');
-		this.call_when = call_when;
+		this.callWhen = callWhen;
 	}
 
 	async do() {
 		super.do();
-		if (this.call_when === 'do') {
-			app.Layers.refresh_gui();
+		
+		if (this.callWhen === 'do') {
+			app.Layers.refreshGUI();
 		}
 	}
 
 	async undo() {
 		super.undo();
-		if (this.call_when === 'undo') {
-			app.Layers.refresh_gui();
+		
+		if (this.callWhen === 'undo') {
+			app.Layers.refreshGUI();
 		}
 	}
 }
