1e1db2d03d5f6c4a7276f174b1d9c4716ad6b8f3
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-19 15:26:59 -0400

    Update bundle.js

--- src/js/actions/bundle.js
+++ src/js/actions/bundle.js
@@ -1,59 +1,66 @@
 import config from '../config.js';
-import { Base_action } from './base.js';
+import {BaseAction} from './base.js';
 
-export class Bundle_action extends Base_action {
+export class BundleAction extends BaseAction {
 	/**
 	 * Groups multiple actions together in the undo/redo history, runs them all at once.
 	 */
-	constructor(bundle_id, bundle_name, actions_to_do) {
-		super(bundle_id, bundle_name);
-		this.actions_to_do = actions_to_do;
+	constructor(bundleID, bundleName, actionsToDo) {
+		super(bundleID, bundleName);
+		this.actionsToDo = actionsToDo;
 	}
 
 	async do() {
 		super.do();
 		let error = null;
 		let i = 0;
-		this.memory_estimate = 0;
-		this.database_estimate = 0;
-		for (i = 0; i < this.actions_to_do.length; i++) {
+		this.memoryEstimate = 0;
+		this.databaseEstimate = 0;
+		
+		for (i = 0; i < this.actionsToDo.length; i++) {
 			try {
-				await this.actions_to_do[i].do();
-				this.memory_estimate += this.actions_to_do[i].memory_estimate;
-				this.database_estimate += this.actions_to_do[i].database_estimate;
+				await this.actionsToDo[i].do();
+				this.memoryEstimate += this.actionsToDo[i].memoryEstimate;
+				this.databaseEstimate += this.actionsToDo[i].databaseEstimate;
 			} catch (e) {
 				error = e;
 				break;
 			}
 		}
+		
 		// One of the actions aborted, undo all previous actions.
 		if (error) {
 			for (i--; i >= 0; i--) {
-				await this.actions_to_do[i].undo();
+				await this.actionsToDo[i].undo();
 			}
+			
 			throw error;
 		}
-		config.need_render = true;
+		
+		config.needRender = true;
 	}
 
 	async undo() {
 		super.undo();
-		this.memory_estimate = 0;
-		this.database_estimate = 0;
-		for (let i = this.actions_to_do.length - 1; i >= 0; i--) {
-			await this.actions_to_do[i].undo();
-			this.memory_estimate += this.actions_to_do[i].memory_estimate;
-			this.database_estimate += this.actions_to_do[i].database_estimate;
+		this.memoryEstimate = 0;
+		this.databaseEstimate = 0;
+		
+		for (let i = this.actionsToDo.length - 1; i >= 0; i--) {
+			await this.actionsToDo[i].undo();
+			this.memoryEstimate += this.actionsToDo[i].memoryEstimate;
+			this.databaseEstimate += this.actionsToDo[i].databaseEstimate;
 		}
-		config.need_render = true;
+		
+		config.needRender = true;
 	}
 
 	free() {
-		if (this.actions_to_do) {
-			for (let action of this.actions_to_do) {
+		if (this.actionsToDo) {
+			for (let action of this.actionsToDo) {
 				action.free();
 			}
-			this.actions_to_do = null;
+			
+			this.actionsToDo = null;
 		}
 	}
 }
