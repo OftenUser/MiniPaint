9f0f6a7bbf27e0171d96a6ab26a06544301abcaf
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-23 14:07:07 -0400

    Update selection.js

--- src/js/modules/edit/selection.js
+++ src/js/modules/edit/selection.js
@@ -1,26 +1,27 @@
 import config from './../../config.js';
-import Base_layers_class from './../../core/base-layers.js';
-import Selection_class from './../../tools/selection.js';
+import BaseLayersClass from './../../core/base-layers.js';
+import SelectionClass from './../../tools/selection.js';
 import alertify from './../../../../node_modules/alertifyjs/build/alertify.min.js';
 
 class Edit_selection_class {
 
 	constructor() {
-		this.Base_layers = new Base_layers_class();
-		this.Selection = new Selection_class(this.Base_layers.ctx);
+		this.BaseLayers = new BaseLayersClass();
+		this.Selection = new SelectionClass(this.BaseLayers.ctx);
 	}
 
-	select_all() {
+	selectAll() {
 		if (config.layer.type != 'image') {
 			alertify.error('This layer must contain an image. Please convert it to raster to apply this tool.');
 			return;
 		}
-		this.Selection.select_all();
+		
+		this.Selection.selectAll();
 	}
 
 	delete() {
-		this.Selection.delete_selection();
+		this.Selection.deleteSelection();
 	}
 }
 
-export default Edit_selection_class;
+export default EditSelectionClass;
