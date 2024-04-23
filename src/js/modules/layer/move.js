e1942e72e4c1f767155b8cea336b80b711780408
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-23 14:18:09 -0400

    Update move.js

--- src/js/modules/layer/move.js
+++ src/js/modules/layer/move.js
@@ -1,24 +1,23 @@
 import app from './../../app.js';
 import config from './../../config.js';
-import Base_layers_class from './../../core/base-layers.js';
+import BaseLayersClass from './../../core/base-layers.js';
 
-class Layer_move_class {
-
+class LayerMoveClass {
 	constructor() {
-		this.Base_layers = new Base_layers_class();
+		this.BaseLayers = new BaseLayersClass();
 	}
 
 	up() {
-		app.State.do_action(
-			new app.Actions.Reorder_layer_action(config.layer.id, 1)
+		app.State.doAction(
+			new app.Actions.ReorderLayerAction(config.layer.id, 1)
 		);
 	}
 
 	down() {
-		app.State.do_action(
-			new app.Actions.Reorder_layer_action(config.layer.id, -1)
+		app.State.doAction(
+			new app.Actions.ReorderLayerAction(config.layer.id, -1)
 		);
 	}
 }
 
-export default Layer_move_class;
+export default LayerMoveClass;
