ca326fe712ac4ec56848154d593984f3e601d5f6
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-23 14:05:23 -0400

    Update copy.js

--- src/js/modules/edit/copy.js
+++ src/js/modules/edit/copy.js
@@ -1,67 +1,68 @@
 import config from "../../config";
-import Base_layers_class from './../../core/base-layers.js';
-import File_save_class from './../file/save.js';
-import Helper_class from './../../libs/helpers.js';
+import BaseLayersClass from './../../core/base-layers.js';
+import FileSaveClass from './../file/save.js';
+import HelperClass from './../../libs/helpers.js';
 import alertify from './../../../../node_modules/alertifyjs/build/alertify.min.js';
 
 var instance = null;
 
-class Copy_class {
+class CopyClass {
 
 	constructor() {
-		//singleton
+		// Singleton
 		if (instance) {
 			return instance;
 		}
+		
 		instance = this;
 
-		this.Base_layers = new Base_layers_class();
-		this.Helper = new Helper_class();
-		this.File_save = new File_save_class();
+		this.BaseLayers = new BaseLayersClass();
+		this.Helper = new HelperClass();
+		this.FileSave = new FileSaveClass();
 
-		//events
+		// Events
 		document.addEventListener('keydown', (event) => {
 			var code = event.key.toLowerCase();
 			var ctrlDown = event.ctrlKey || event.metaKey;
-			if (this.Helper.is_input(event.target))
+			
+			if (this.Helper.isInput(event.target))
 				return;
 
 			if (code == "c" && ctrlDown == true) {
-				//copy to clipboard
-				this.copy_to_clipboard();
+				// Copy to clipboard
+				this.copyToClipboard();
 			}
 		}, false);
 	}
 
-	async copy_to_clipboard(){
+	async copyToClipboard() {
 		var _this = this;
 
 		const canWriteToClipboard = await this.askWritePermission();
 		if (canWriteToClipboard) {
 
-			//get data - current layer
-			var canvas = this.Base_layers.convert_layer_to_canvas();
+			// Get data - Current layer
+			var canvas = this.BaseLayers.convertLayerToCanvas();
 			var ctx = canvas.getContext("2d");
 
 			if (config.TRANSPARENCY == false) {
-				//add white background
+				// Add white background
 				ctx.globalCompositeOperation = 'destination-over';
-				this.File_save.fillCanvasBackground(ctx, '#ffffff');
+				this.FileSave.fillCanvasBackground(ctx, '#FFFFFF');
 				ctx.globalCompositeOperation = 'source-over';
 			}
 
-			//save using lib
-			canvas.toBlob(function (blob) {
+			// Save using lib
+			canvas.toBlob(function(blob) {
 				_this.setToClipboard(blob);
 			});
-		}
-		else{
+		} else {
 			alertify.error('Missing permissions to write to Clipboard.cc');
 		}
 	}
 
 	async setToClipboard(blob) {
-		const data = [new ClipboardItem({ [blob.type]: blob })];
+		const data = [new ClipboardItem({[blob.type]: blob})];
 		await navigator.clipboard.write(data);
 	}
 
@@ -69,14 +70,13 @@ class Copy_class {
 		try {
 			// The clipboard-write permission is granted automatically to pages
 			// when they are the active tab. So it's not required, but it's more safe.
-			const { state } = await navigator.permissions.query({ name: 'clipboard-write' })
+			const {state} = await navigator.permissions.query({name: 'clipboard-write'})
 			return state === 'granted';
-		}
-		catch (error) {
+		} catch (error) {
 			// Browser compatibility / Security error (ONLY HTTPS) ...
 			return false;
 		}
 	}
 }
 
-export default Copy_class;
+export default CopyClass;
