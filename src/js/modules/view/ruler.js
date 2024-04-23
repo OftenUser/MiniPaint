9ae6107115d6e5ed89849fc6efe3f8303093c0e4
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-23 14:36:28 -0400

    Update ruler.js

--- src/js/modules/view/ruler.js
+++ src/js/modules/view/ruler.js
@@ -1,38 +1,41 @@
 import config from './../../config.js';
-import Helper_class from './../../libs/helpers.js';
-import Base_gui_class from './../../core/base-gui.js';
-import Base_layers_class from './../../core/base-layers.js';
-import Tools_settings_class from './../tools/settings.js';
+import HelperClass from './../../libs/helpers.js';
+import BaseGUIClass from './../../core/base-gui.js';
+import BaseLayersClass from './../../core/base-layers.js';
+import ToolsSettingsClass from './../tools/settings.js';
 
 var instance = null;
 
-class View_ruler_class {
+class ViewRulerClass {
 
 	constructor() {
-		//singleton
+		// Singleton
 		if (instance) {
 			return instance;
 		}
+		
 		instance = this;
 
-		this.GUI = new Base_gui_class();
-		this.Base_layers = new Base_layers_class();
-		this.Tools_settings = new Tools_settings_class();
-		this.Helper = new Helper_class();
+		this.GUI = new BaseGUIClass();
+		this.BaseLayers = new BaseLayersClass();
+		this.ToolsSettings = new ToolsSettingsClass();
+		this.Helper = new HelperClass();
 
-		this.set_events();
+		this.setEvents();
 	}
 
-	set_events() {
+	setEvents() {
 		var _this = this;
 
-		window.addEventListener('resize', function (event) {
-			//resize
-			_this.prepare_ruler();
-			_this.render_ruler();
+		window.addEventListener('resize', function(event) {
+			// Resize
+			_this.prepareRuler();
+			_this.renderRuler();
 		}, false);
+		
 		document.addEventListener('keydown', (event) => {
 			var code = event.code;
+			
 			if (this.Helper.is_input(event.target))
 				return;
 
@@ -44,162 +47,171 @@ class View_ruler_class {
 	}
 
 	ruler() {
-		var ruler_left = document.getElementById('ruler_left');
-		var ruler_top = document.getElementById('ruler_top');
-		var middle_area = document.getElementById('middle_area');
+		var rulerLeft = document.getElementById('ruler_left');
+		var rulerTop = document.getElementById('ruler_top');
+		var middleArea = document.getElementById('middle_area');
 
-		if(config.ruler_active == false){
-			//activate
-			config.ruler_active = true;
+		if (config.ruler_active == false) {
+			// Activate
+			config.rulerActive = true;
 			document.getElementById('middle_area').classList.add('has-ruler');
-			ruler_left.style.display = 'block';
-			ruler_top.style.display = 'block';
+			rulerLeft.style.display = 'block';
+			rulerTop.style.display = 'block';
 
-			this.prepare_ruler();
-			this.render_ruler();
-		}
-		else{
-			//deactivate
-			config.ruler_active = false;
+			this.prepareRuler();
+			this.renderRuler();
+		} else {
+			// Deactivate
+			config.rulerActive = false;
 			document.getElementById('middle_area').classList.remove('has-ruler');
-			ruler_left.style.display = 'none';
-			ruler_top.style.display = 'none';
+			rulerLeft.style.display = 'none';
+			rulerTop.style.display = 'none';
 		}
 
-		this.GUI.prepare_canvas();
+		this.GUI.prepareCanvas();
 
-		config.need_render = true;
+		config.needRender = true;
 	}
 
-	prepare_ruler(){
-		if(config.ruler_active == false)
+	prepareRuler() {
+		if (config.rulerActive == false)
 			return;
 
-		var ruler_left = document.getElementById('ruler_left');
-		var ruler_top = document.getElementById('ruler_top');
-		var middle_area = document.getElementById('middle_area');
+		var rulerLeft = document.getElementById('ruler_left');
+		var rulerTop = document.getElementById('ruler_top');
+		var middleArea = document.getElementById('middle_area');
 
-		var middle_area_width = middle_area.clientWidth;
-		var middle_area_height = middle_area.clientHeight;
+		var middleAreaWidth = middleArea.clientWidth;
+		var middleAreaHeight = middleArea.clientHeight;
 
-		ruler_left.width = 15;
-		ruler_left.height = middle_area_height - 20;
+		rulerLeft.width = 15;
+		rulerLeft.height = middleAreaHeight - 20;
 
-		ruler_top.width = middle_area_width - 20;
-		ruler_top.height = 15;
+		rulerTop.width = middleAreaWidth - 20;
+		rulerTop.height = 15;
 	}
 
-	render_ruler(){
-		if(config.ruler_active == false)
+	renderRuler() {
+		if (config.rulerActive == false)
 			return;
 
-		var units = this.Tools_settings.get_setting('default_units');
-		var resolution = this.Tools_settings.get_setting('resolution');
+		var units = this.ToolsSettings.getSetting('default_units');
+		var resolution = this.ToolsSettings.getSetting('resolution');
 
-		var ruler_left = document.getElementById('ruler_left');
-		var ruler_top = document.getElementById('ruler_top');
+		var rulerLeft = document.getElementById('ruler_left');
+		var rulerTop = document.getElementById('ruler_top');
 
-		var ctx_left = ruler_left.getContext("2d");
-		var ctx_top = ruler_top.getContext("2d");
+		var ctxLeft = ruler_left.getContext("2d");
+		var ctxTop = ruler_top.getContext("2d");
 
 		var color = '#111';
 		var size = 15;
 
-		//calc step
+		// Calculate step
 		var step = Math.ceil(10 * config.ZOOM);
+		
 		while (step < 5) {
 			step = step * 2;
 		}
+		
 		while (step > 10) {
 			step = Math.ceil(step / 2);
 		}
-		var step_big = step * 10;
+		
+		var stepBig = step * 10;
 
-		//calc begin/end point
-		var begin_x = Math.max(0, ruler_top.width / 2 - config.WIDTH * config.ZOOM / 2);
-		var begin_y = Math.max(0, ruler_left.height / 2 - config.HEIGHT * config.ZOOM / 2);
+		// Calculate begin/end point
+		var beginX = Math.max(0, rulerTop.width / 2 - config.WIDTH * config.ZOOM / 2);
+		var beginY = Math.max(0, rulerLeft.height / 2 - config.HEIGHT * config.ZOOM / 2);
 
-		var end_x = Math.min(ruler_top.width, ruler_top.width / 2 + config.WIDTH * config.ZOOM / 2);
-		var end_y = Math.min(ruler_left.height, ruler_left.height / 2 + config.HEIGHT * config.ZOOM / 2);
+		var endX = Math.min(rulerTop.width, rulerTop.width / 2 + config.WIDTH * config.ZOOM / 2);
+		var endY = Math.min(rulerLeft.height, rulerLeft.height / 2 + config.HEIGHT * config.ZOOM / 2);
 
-		//left
-		ctx_left.strokeStyle = color;
-		ctx_left.lineWidth = 1;
-		ctx_left.font = "11px Arial";
+		// Left
+		ctxLeft.strokeStyle = color;
+		ctxLeft.lineWidth = 1;
+		ctxLeft.font = "11px Arial";
 
-		ctx_left.clearRect(0, 0, ruler_left.width, ruler_left.height);
+		ctxLeft.clearRect(0, 0, rulerLeft.width, rulerLeft.height);
 
-		ctx_left.beginPath();
-		for (var i = begin_y; i < end_y; i += step) {
-			ctx_left.moveTo(10, i + 0.5);
-			ctx_left.lineTo(size, i + 0.5);
+		ctxLeft.beginPath();
+		
+		for (var i = beginY; i < endY; i += step) {
+			ctxLeft.moveTo(10, i + 0.5);
+			ctxLeft.lineTo(size, i + 0.5);
 		}
-		ctx_left.stroke();
+		
+		ctxLeft.stroke();
 
-		ctx_left.beginPath();
-		for (var i = begin_y; i <= end_y; i += step_big) {
-			ctx_left.moveTo(0, i + 0.5);
-			ctx_left.lineTo(size, i + 0.5);
+		ctxLeft.beginPath();
+		
+		for (var i = beginY; i <= endY; i += stepBig) {
+			ctxLeft.moveTo(0, i + 0.5);
+			ctxLeft.lineTo(size, i + 0.5);
 
-			var global_pos = this.Base_layers.get_world_coords(0, i - begin_y);
-			var value = this.Helper.get_user_unit(global_pos.y, units, resolution);
+			var globalPos = this.BaseLayers.getWorldCoordinates(0, i - beginY);
+			var value = this.Helper.getUserUnit(globalPos.y, units, resolution);
 
-			if(units == 'inches'){
-				//more decimals value
-				var text = this.Helper.number_format(value, 1);
-			}
-			else{
+			if (units == 'inches') {
+				// More decimals value
+				var text = this.Helper.numberFormat(value, 1);
+			} else {
 				var text = Math.ceil(value);
 			}
+			
 			text = text.toString();
 
-			//text
+			// Text
 			for (var j = 0; j < text.length; j++) {
 				var letter = text.charAt(j);
-				var line_height = 10;
-				ctx_left.fillText(letter, 1, i + 11 + j * line_height);
+				var lineHeight = 10;
+				ctxLeft.fillText(letter, 1, i + 11 + j * lineHeight);
 			}
 		}
-		ctx_left.stroke();
+		
+		ctxLeft.stroke();
 
-		//top
-		ctx_top.strokeStyle = color;
-		ctx_top.lineWidth = 1;
-		ctx_top.font = "11px Arial";
+		// Top
+		ctxLeft.strokeStyle = color;
+		ctxTop.lineWidth = 1;
+		ctxTop.font = "11px Arial";
 
-		ctx_top.clearRect(0, 0, ruler_top.width, ruler_top.height);
+		ctxTop.clearRect(0, 0, rulerTop.width, rulerTop.height);
 
-		ctx_top.beginPath();
-		for (var i = begin_x; i < end_x; i += step) {
-			var y = (i / step_big == parseInt(i / step_big)) ? 0 : step;
-			ctx_top.moveTo(i + 0.5, 10);
-			ctx_top.lineTo(i + 0.5, size);
+		ctxTop.beginPath();
+		
+		for (var i = beginX; i < endX; i += step) {
+			var y = (i / stepBig == parseInt(i / stepBig)) ? 0 : step;
+			ctxTop.moveTo(i + 0.5, 10);
+			ctxTop.lineTo(i + 0.5, size);
 		}
-		ctx_top.stroke();
+		
+		ctxTop.stroke();
 
-		ctx_top.beginPath();
-		for (var i = begin_x; i <= end_x; i += step_big) {
-			ctx_top.moveTo(i + 0.5, 0);
-			ctx_top.lineTo(i + 0.5, size);
+		ctxTop.beginPath();
+		
+		for (var i = beginX; i <= endX; i += stepBig) {
+			ctxTop.moveTo(i + 0.5, 0);
+			ctxTop.lineTo(i + 0.5, size);
 
-			var global_pos = this.Base_layers.get_world_coords(i - begin_x, 0);
-			var value = this.Helper.get_user_unit(global_pos.x, units, resolution);
+			var globalPos = this.BaseLayers.getWorldCoordinates(i - beginX, 0);
+			var value = this.Helper.getUserUnit(globalPos.x, units, resolution);
 
-			if(units == 'inches'){
-				//more decimals value
-				var text = this.Helper.number_format(value, 1);
-			}
-			else{
+			if (units == 'inches') {
+				// More decimals value
+				var text = this.Helper.numberFormat(value, 1);
+			} else {
 				var text = Math.ceil(value);
 			}
+			
 			text = text.toString();
 
-			//text
-			ctx_top.fillText(text, i + 3, 9);
+			// Text
+			ctxTop.fillText(text, i + 3, 9);
 		}
-		ctx_top.stroke();
+		
+		ctxTop.stroke();
 	}
-
 }
 
-export default View_ruler_class;
+export default ViewRulerClass;
