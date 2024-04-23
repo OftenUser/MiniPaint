914334392d4f39722856bf25f6f07b8634e57e39
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-23 14:13:28 -0400

    Update opacity.js

--- src/js/modules/image/opacity.js
+++ src/js/modules/image/opacity.js
@@ -1,56 +1,59 @@
 import app from './../../app.js';
 import config from './../../config.js';
-import Dialog_class from './../../libs/popup.js';
+import DialogClass from './../../libs/popup.js';
 
-class Image_opacity_class {
+class ImageOpacityClass {
 
 	constructor() {
-		this.POP = new Dialog_class();
+		this.POP = new DialogClass();
 	}
 
 	opacity() {
 		var _this = this;
-		var initial_opacity = config.layer.opacity;
+		var initialOpacity = config.layer.opacity;
 
 		var settings = {
 			title: 'Opacity',
 			params: [
 				{name: "opacity", title: "Alpha:", value: config.layer.opacity, range: [0, 100]},
 			],
-			on_change: function (params, canvas_preview, w, h) {
-				_this.opacity_handler(params, false);
+			on_change: function(params, canvasPreview, _width, _height) {
+				_this.opacityHandler(params, false);
 			},
-			on_finish: function (params) {
-				config.layer.opacity = initial_opacity;
-				_this.opacity_handler(params);
+			on_finish: function(params) {
+				config.layer.opacity = initialOpacity;
+				_this.opacityHandler(params);
 			},
-			on_cancel: function (params) {
-				config.layer.opacity = initial_opacity;
-				config.need_render = true;
+			on_cancel: function(params) {
+				config.layer.opacity = initialOpacity;
+				config.needRender = true;
 			},
 		};
+		
 		this.POP.show(settings);
 	}
 
-	opacity_handler(data, is_final = true) {
+	opacityHandler(data, isFinal = true) {
 		var value = parseInt(data.opacity);
 		if (value < 0)
 			value = 0;
+		
 		if (value > 100)
 			value = 100;
-		if (is_final) {
-			app.State.do_action(
-				new app.Actions.Bundle_action('change_opacity', 'Change Opacity', [
-					new app.Actions.Update_layer_action(config.layer.id, {
+		
+		if (isFinal) {
+			app.State.doAction(
+				new app.Actions.BundleAction('change_opacity', 'Change Opacity', [
+					new app.Actions.UpdateLayerAction(config.layer.id, {
 						opacity: value
 					})
 				])
 			);
 		} else {
 			config.layer.opacity = value;
-			config.need_render = true;
+			config.needRender = true;
 		}
 	}
 }
 
-export default Image_opacity_class;
+export default ImageOpacityClass;
