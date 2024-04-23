4f591f8be7cef8cbeceb642de030751acdb088b1
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-23 14:16:09 -0400

    Update clear.js

--- src/js/modules/layer/clear.js
+++ src/js/modules/layer/clear.js
@@ -1,19 +1,19 @@
 import app from './../../app.js';
 import config from './../../config.js';
-import Base_layers_class from './../../core/base-layers.js';
+import BaseLayersClass from './../../core/base-layers.js';
 
-class Layer_clear_class {
+class LayerClearClass {
 
 	constructor() {
-		this.Base_layers = new Base_layers_class();
+		this.BaseLayers = new BaseLayersClass();
 	}
 
 	clear() {
-		return app.State.do_action(
-			new app.Actions.Clear_layer_action(config.layer.id)
+		return app.State.doAction(
+			new app.Actions.ClearLayerAction(config.layer.id)
 		);
 	}
 
 }
 
-export default Layer_clear_class;
+export default LayerClearClass;
