3b8035409b112ab47342160fd91296f1dfed89c2
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-27 11:32:42 -0400

    Update desaturate.js

--- src/js/tools/desaturate.js
+++ src/js/tools/desaturate.js
@@ -1,17 +1,16 @@
 import app from './../app.js';
 import config from './../config.js';
-import Base_tools_class from './../core/base-tools.js';
-import Base_layers_class from './../core/base-layers.js';
+import BaseToolsClass from './../core/base-tools.js';
+import BaseLayersClass from './../core/base-layers.js';
 import alertify from './../../../node_modules/alertifyjs/build/alertify.min.js';
 import ImageFilters from './../libs/imagefilters.js';
-import Helper_class from './../libs/helpers.js';
+import HelperClass from './../libs/helpers.js';
 
-class Desaturate_class extends Base_tools_class {
-
+class DesaturateClass extends BaseToolsClass {
 	constructor(ctx) {
 		super();
-		this.Base_layers = new Base_layers_class();
-		this.Helper = new Helper_class();
+		this.BaseLayers = new BaseLayersClass();
+		this.Helper = new HelperClass();
 		this.ctx = ctx;
 		this.name = 'desaturate';
 		this.tmpCanvas = null;
@@ -20,116 +19,125 @@ class Desaturate_class extends Base_tools_class {
 	}
 
 	load() {
-		this.default_events();
+		this.defaultEvents();
 	}
 
-	default_dragMove(event) {
+	defaultDragMove(event) {
 		if (config.TOOL.name != this.name)
 			return;
+		
 		this.mousemove(event);
 
-		//mouse cursor
-		var mouse = this.get_mouse_info(event);
+		// Mouse cursor
+		var mouse = this.getMouseInfo(event);
 		var params = this.getParams();
-		this.show_mouse_cursor(mouse.x, mouse.y, params.size, 'circle');
+		this.showMouseCursor(mouse.x, mouse.y, params.size, 'circle');
 	}
 
 	mousedown(e) {
 		this.started = false;
-		var mouse = this.get_mouse_info(e);
+		var mouse = this.getMouseInfo(e);
 		var params = this.getParams();
-		if (mouse.click_valid == false) {
+		
+		if (mouse.clickValid == false) {
 			return;
 		}
+		
 		if (config.layer.type != 'image') {
 			alertify.error('This layer must contain an image. Please convert it to raster to apply this tool.');
 			return;
 		}
+		
 		if (config.layer.rotate || 0 > 0) {
 			alertify.error('Erase on rotate object is disabled. Please rasterize first.');
 			return;
 		}
+		
 		this.started = true;
 
-		//get canvas from layer
+		// Get canvas from layer
 		this.tmpCanvas = document.createElement('canvas');
 		this.tmpCanvasCtx = this.tmpCanvas.getContext("2d");
-		this.tmpCanvas.width = config.layer.width_original;
-		this.tmpCanvas.height = config.layer.height_original;
+		this.tmpCanvas.width = config.layer.widthOriginal;
+		this.tmpCanvas.height = config.layer.heightOriginal;
 
 		this.tmpCanvasCtx.drawImage(config.layer.link, 0, 0);
 
-		//do desaturate
-		this.desaturate_general('click', mouse, params.size, params.anti_aliasing);
+		// Do desaturate
+		this.desaturateGeneral('click', mouse, params.size, params.antiAliasing);
 
-		//register tmp canvas for faster redraw
-		config.layer.link_canvas = this.tmpCanvas;
-		config.need_render = true;
+		// Register tmp canvas for faster redraw
+		config.layer.linkCanvas = this.tmpCanvas;
+		config.needRender = true;
 	}
 
 	mousemove(e) {
-		var mouse = this.get_mouse_info(e);
+		var mouse = this.getMouseInfo(e);
 		var params = this.getParams();
-		if (mouse.is_drag == false)
+		
+		if (mouse.isDrag == false)
 			return;
-		if (mouse.click_valid == false) {
+		
+		if (mouse.clickValid == false) {
 			return;
 		}
+		
 		if (this.started == false) {
 			return;
 		}
 
-		//do desaturate
-		this.desaturate_general('move', mouse, params.size, params.anti_aliasing);
+		// Do desaturate
+		this.desaturateGeneral('move', mouse, params.size, params.antiAliasing);
 
-		//draw draft preview
-		config.need_render = true;
+		// Draw draft preview
+		config.needRender = true;
 	}
 
 	mouseup(e) {
 		if (this.started == false) {
 			return;
 		}
-		delete config.layer.link_canvas;
+		
+		delete config.layer.linkCanvas;
 
-		app.State.do_action(
-			new app.Actions.Bundle_action('desaturate_tool', 'Desaturate Tool', [
-				new app.Actions.Update_layer_image_action(this.tmpCanvas)
+		app.State.doAction(
+			new app.Actions.BundleAction('desaturate_tool', 'Desaturate Tool', [
+				new app.Actions.UpdateLayerImageAction(this.tmpCanvas)
 			])
 		);
 
-		//decrease memory
+		// Decrease memory
 		this.tmpCanvas.width = 1;
 		this.tmpCanvas.height = 1;
 		this.tmpCanvas = null;
 		this.tmpCanvasCtx = null;
 	}
 
-	desaturate_general(type, mouse, size, anti_aliasing) {
+	desaturate_general(type, mouse, size, antiAliasing) {
 		var ctx = this.tmpCanvasCtx;
-		var mouse_x = Math.round(mouse.x) - config.layer.x;
-		var mouse_y = Math.round(mouse.y) - config.layer.y;
+		var mouseX = Math.round(mouse.x) - config.layer.x;
+		var mouseY = Math.round(mouse.y) - config.layer.y;
 
-		//adapt to origin size
-		mouse_x = this.adaptSize(mouse_x, 'width');
-		mouse_y = this.adaptSize(mouse_y, 'height');
-		var size_w = this.adaptSize(size, 'width');
-		var size_h = this.adaptSize(size, 'height');
+		// Adapt to origin size
+		mouseX = this.adaptSize(mouseX, 'width');
+		mouseY = this.adaptSize(mouseY, 'height');
+		var sizeWidth = this.adaptSize(size, 'width');
+		var sizeHeight = this.adaptSize(size, 'height');
 
-		//find center
-		var center_x = mouse_x - Math.round(size_w / 2);
-		var center_y = mouse_y - Math.round(size_h / 2);
+		// Find center
+		var centerX = mouseX - Math.round(size_w / 2);
+		var centerY = mouseY - Math.round(size_h / 2);
 
-		//convert float coords to integers
-		center_x = Math.round(center_x);
-		center_y = Math.round(center_y);
-		mouse_x = Math.round(mouse_x);
-		mouse_y = Math.round(mouse_y);
+		// Convert float coordinates to integers
+		centerX = Math.round(centerX);
+		centerY = Math.round(centerY);
+		mouseX = Math.round(mouseX);
+		mouseY = Math.round(mouseY);
 
-		var imageData = ctx.getImageData(center_x, center_y, size_w, size_h);
-		var filtered = ImageFilters.GrayScale(imageData); //add effect
-		this.Helper.image_round(this.tmpCanvasCtx, mouse_x, mouse_y, size_w, size_h, filtered, anti_aliasing);
+		var imageData = ctx.getImageData(centerX, centerY, sizeWidth, sizeHeight);
+		var filtered = ImageFilters.GrayScale(imageData); // Add effect
+		this.Helper.imageRound(this.tmpCanvasCtx, mouseX, mouseY, sizeWidth, sizeHeight, filtered, antiAliasing);
 	}
-
 }
-export default Desaturate_class;
+
+export default DesaturateClass;
