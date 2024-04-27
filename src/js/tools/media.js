1492f85bd8242884022bfce0598e634e57d5f7f8
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2024-04-27 11:38:12 -0400

    Update media.js

--- src/js/tools/media.js
+++ src/js/tools/media.js
@@ -1,38 +1,37 @@
 import app from './../app.js';
 import config from './../config.js';
-import Base_tools_class from './../core/base-tools.js';
-import File_open_class from './../modules/file/open.js';
-import Tools_settings_class from './../modules/tools/settings.js';
-import Dialog_class from './../libs/popup.js';
+import BaseToolsClass from './../core/base-tools.js';
+import FileOpenClass from './../modules/file/open.js';
+import ToolsSettingsClass from './../modules/tools/settings.js';
+import DialogClass from './../libs/popup.js';
 import alertify from './../../../node_modules/alertifyjs/build/alertify.min.js';
 
-class Media_class extends Base_tools_class {
-
+class MediaClass extends BaseToolsClass {
 	constructor(ctx) {
 		super();
-		this.File_open = new File_open_class();
-		this.Tools_settings = new Tools_settings_class();
-		this.POP = new Dialog_class();
+		this.FileOpen = new FileOpenClass();
+		this.ToolsSettings = new ToolsSettingsClass();
+		this.POP = new DialogClass();
 		this.name = 'media';
 		this.cache = [];
 		this.page = 1;
-		this.per_page = 50;
+		this.perPage = 50;
 	}
 
 	load() {
-		//nothing
+		// Nothing
 	}
 
 	render(ctx, layer) {
-		//nothing
+		// Nothing
 	}
 
-	on_activate() {
+	onActivate() {
 		this.search();
 	}
 
 	/**
-	 * Image search api
+	 * Image search API
 	 *
 	 * @param {string} query
 	 * @param {array} data
@@ -41,12 +40,12 @@ class Media_class extends Base_tools_class {
 	search(query = '', data = [], pages = null) {
 		var _this = this;
 		var html = '';
-		var html_paging = '';
+		var htmlPaging = '';
 
-		var key = config.pixabay_key;
+		var key = config.pixabayKey;
 		key = key.split("").reverse().join("");
 
-		var safe_search = this.Tools_settings.get_setting('safe_search');
+		var safeSearch = this.ToolsSettings.getSetting('safe_search');
 
 		if (data.length > 0) {
 			for (var i in data) {
@@ -54,99 +53,105 @@ class Media_class extends Base_tools_class {
 				html += '	<img class="displayBlock pointer" alt="" src="' + data[i].previewURL + '" data-url="' + data[i].webformatURL + '" />';
 				html += '</div>';
 			}
-			//fix for last line
+			
+			// Fix for last line
 			html += '<div class="item"></div>';
 			html += '<div class="item"></div>';
 			html += '<div class="item"></div>';
 			html += '<div class="item"></div>';
 
-			//paging
-			html_paging += '<div class="media-paging" id="media_paging">';
-			html_paging += '<button type="button" data-value="1" title="Previous">&lt;</button> ';
-			for(var i = 1; i <= Math.min(10, pages); i++) {
+			// Paging
+			htmlPaging += '<div class="media-paging" id="media_paging">';
+			htmlPaging += '<button type="button" data-value="1" title="Previous">&lt;</button> ';
+			
+			for (var i = 1; i <= Math.min(10, pages); i++) {
 				var selected = '';
-				if(this.page == i){
+				
+				if (this.page == i) {
 					var selected = 'selected';
 				}
-				html_paging += '<button type="button" class="'+selected+'" data-value="'+i+'">'+i+'</button> ';
+				
+				htmlPaging += '<button type="button" class="' + selected + '" data-value="' + i + '" title="' + i + '">' + i + '</button> ';
 			}
-			html_paging += '<button type="button" data-value="'+Math.min(this.page + 1, pages)+'" title="Next">&gt;</button> ';
-			html_paging += '</div>';
-		}
-		else{
+			
+			htmlPaging += '<button type="button" data-value="' + Math.min(this.page + 1, pages) + '" title="Next">&gt;</button> ';
+			htmlPaging += '</div>';
+		} else {
 			this.page = 1;
 		}
 
 		var settings = {
 			title: 'Search',
-			//comment: 'Source: <a class="text_muted" href="https://pixabay.com/">pixabay.com</a>.',
+			// comment: 'Source: <a class="text_muted" href="https://pixabay.com/" title="Pixabay.com">Pixabay.com</a>.',
 			className: 'wide',
 			params: [
 				{name: "query", title: "Keyword:", value: query},
 			],
-			on_load: function (params, popup) {
+			on_load: function(params, popup) {
 				var node = document.createElement("div");
 				node.classList.add('flex-container');
-				node.innerHTML = html + html_paging;
+				node.innerHTML = html + htmlPaging;
 				popup.el.querySelector('.dialog_content').appendChild(node);
-				//events
+				
+				// Events
 				var targets = popup.el.querySelectorAll('.item img');
+				
 				for (var i = 0; i < targets.length; i++) {
-					targets[i].addEventListener('click', function (event) {
-						//we have click
+					targets[i].addEventListener('click', function(event) {
+						// We have click
 						var data = {
 							url: this.dataset.url,
 						};
-						_this.File_open.file_open_url_handler(data);
+						_this.FileOpen.fileOpenURLHandler(data);
 						_this.POP.hide();
 
-						new app.Actions.Activate_tool_action('select', true).do();
+						new app.Actions.ActivateToolAction('select', true).do();
 					});
 				}
+				
 				var targets = popup.el.querySelectorAll('#media_paging button');
+				
 				for (var i = 0; i < targets.length; i++) {
-					targets[i].addEventListener('click', function (event) {
-						//we have click
+					targets[i].addEventListener('click', function(event) {
+						// We have click
 						_this.page = parseInt(this.dataset.value);
 						_this.POP.save();
 					});
 				}
 			},
-			on_finish: function (params) {
+			on_finish: function(params) {
 				if (params.query == '')
 					return;
 
 				var URL = "https://pixabay.com/api/?key=" + key
 					+ "&page=" + _this.page
-					+ "&per_page=" + _this.per_page
+					+ "&per_page=" + _this.perPage
 					+ "&safesearch=" + safe_search
 					+ "&q="	+ encodeURIComponent(params.query);
 
 				if (_this.cache[URL] != undefined) {
-					//using cache
-
-					setTimeout(function () {
-						//only call same function after all handlers finishes
+					// Using cache
+					setTimeout(function() {
+						// Only call same function after all handlers finishes
 						var data = _this.cache[URL];
 
 						if (parseInt(data.totalHits) == 0) {
 							alertify.error('Your search did not match any images.');
 						}
 
-						var pages = Math.ceil(data.totalHits / _this.per_page);
+						var pages = Math.ceil(data.totalHits / _this.perPage);
 						_this.search(params.query, data.hits, pages);
 					}, 100);
-				}
-				else {
-					//query to service
-					$.getJSON(URL, function (data) {
+				} else {
+					// Query to service
+					$.getJSON(URL, function(data) {
 						_this.cache[URL] = data;
 
 						if (parseInt(data.totalHits) == 0) {
 							alertify.error('Your search did not match any images.');
 						}
 
-						var pages = Math.ceil(data.totalHits / _this.per_page);
+						var pages = Math.ceil(data.totalHits / _this.perPage);
 						_this.search(params.query, data.hits, pages);
 					})
 					.fail(function () {
@@ -155,10 +160,11 @@ class Media_class extends Base_tools_class {
 				}
 			},
 		};
+		
 		this.POP.show(settings);
 
 		document.getElementById("pop_data_query").select();
 	}
 }
 
-export default Media_class;
+export default MediaClass;
